815aad5d7787b5738c4fd97c7c4a177d94996a94
Umran Ozman <umonal@yahoo.com>
2024-12-25 10:32:56 -0500

    ConfigCourse Class updated

--- Spring-01-IoC/src/main/java/com/cydeo/dependency_injection/ConfigCourse.java
+++ Spring-01-IoC/src/main/java/com/cydeo/dependency_injection/ConfigCourse.java
@@ -1,4 +1,9 @@
 package com.cydeo.dependency_injection;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+
+@Configuration
+@ComponentScan
 public class ConfigCourse {
 }
