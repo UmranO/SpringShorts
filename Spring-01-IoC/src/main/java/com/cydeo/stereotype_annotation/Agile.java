e158e3ce988696be4ce60a59dfea05d475d48ed3
Umran Ozman <umonal@yahoo.com>
2024-12-24 20:57:34 -0500

    Agile Class updated

--- Spring-01-IoC/src/main/java/com/cydeo/stereotype_annotation/Agile.java
+++ Spring-01-IoC/src/main/java/com/cydeo/stereotype_annotation/Agile.java
@@ -1,5 +1,7 @@
 package com.cydeo.stereotype_annotation;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Agile {
     public void getTeachingHours(){
         System.out.println("Weekly teaching hours : " + 10);
