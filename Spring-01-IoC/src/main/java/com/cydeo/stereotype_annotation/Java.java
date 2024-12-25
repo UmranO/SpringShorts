4f903ce33c0a9e834a019f7b6dbbe4d3effcc591
Umran Ozman <umonal@yahoo.com>
2024-12-24 20:58:00 -0500

    Java Class updated

--- Spring-01-IoC/src/main/java/com/cydeo/stereotype_annotation/Java.java
+++ Spring-01-IoC/src/main/java/com/cydeo/stereotype_annotation/Java.java
@@ -1,5 +1,7 @@
 package com.cydeo.stereotype_annotation;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Java {
     public void getTeachingHours(){
         System.out.println("Weekly teaching hours : " + 20);
