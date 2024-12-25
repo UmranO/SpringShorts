62fed01a2c9a35aad28b7abf250599c7dbafbd11
Umran Ozman <umonal@yahoo.com>
2024-12-25 15:55:29 -0500

    CydeoApp Class updated

--- Spring-01-IoC/src/main/java/com/cydeo/dependency_injection/CydeoApp.java
+++ Spring-01-IoC/src/main/java/com/cydeo/dependency_injection/CydeoApp.java
@@ -1,12 +1,19 @@
 package com.cydeo.dependency_injection;
 
-import com.cydeo.stereotype_annotation.ConfigCourse;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class CydeoApp {
     public static void main(String[] args) {
         ApplicationContext container=new AnnotationConfigApplicationContext(ConfigCourse.class);
+
+        Selenium selenium = container.getBean(Selenium.class);
+        selenium.getTeachingHours();
+
+        Java java = container.getBean(Java.class);
+        java.getTeachingHours();
     }
 }
+
+
 
