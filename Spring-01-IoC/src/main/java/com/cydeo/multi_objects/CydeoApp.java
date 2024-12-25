390a75bf3e045cc66287187fcd84c6790f56401a
Umran Ozman <umonal@yahoo.com>
2024-12-24 19:41:11 -0500

    CydeoApp Class updated

--- Spring-01-IoC/src/main/java/com/cydeo/multi_objects/CydeoApp.java
+++ Spring-01-IoC/src/main/java/com/cydeo/multi_objects/CydeoApp.java
@@ -6,7 +6,7 @@ public class CydeoApp {
     public static void main(String[] args) {
         ApplicationContext container= new AnnotationConfigApplicationContext(CarConfig.class);
 
-        Car car1 = container.getBean(Car.class);
+        Car car1 = container.getBean("c2", Car.class);
         System.out.println(car1.getMake());         //Audi
 
 
