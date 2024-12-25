5342575f517b11cd8cd49481a93c7c72ccd63db8
Umran Ozman <umonal@yahoo.com>
2024-12-25 15:54:05 -0500

    Java Class created

--- Spring-01-IoC/src/main/java/com/cydeo/dependency_injection/Java.java
+++ Spring-01-IoC/src/main/java/com/cydeo/dependency_injection/Java.java
@@ -1,8 +1,18 @@
 package com.cydeo.dependency_injection;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Java {public void getTeachingHours(){
-    System.out.println("Weekly teaching hours : " + 20);
-}
+public class Java {
+
+    OfficeHours officeHours;
+
+   // @Autowired
+    public Java(OfficeHours officeHours) {
+        this.officeHours = officeHours;
+    }
+
+    public void getTeachingHours(){
+        System.out.println("Weekly teaching hours : " + (20 + officeHours.getHours()));
+    }
 }
