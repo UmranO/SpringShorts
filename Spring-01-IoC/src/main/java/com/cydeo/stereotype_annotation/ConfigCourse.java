8ed6e5e5ac15614ae1342253d97b14ec48485cdd
Umran Ozman <umonal@yahoo.com>
2024-12-25 11:11:42 -0500

    ConfigCourse Class updated

--- Spring-01-IoC/src/main/java/com/cydeo/stereotype_annotation/ConfigCourse.java
+++ Spring-01-IoC/src/main/java/com/cydeo/stereotype_annotation/ConfigCourse.java
@@ -3,6 +3,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "com.cydeo")
-public class ConfigCourse {
+@ComponentScan //(basePackages = "com.cydeo")    Sonradan commented out bec. hem bunsuz baska package da olan bir class'in
+public class ConfigCourse {                    //search edilmeyecegini gostermek hem de dependency_injection package'daki
+                                               //java ile interfere edecegi icin
 }
