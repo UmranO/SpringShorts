863d88aca1b28990827a9361e5dedd18d5da2eff
Umran Ozman <umonal@yahoo.com>
2024-12-25 10:10:40 -0500

    CydeApp Class updated

--- Spring-01-IoC/src/main/java/com/cydeo/stereotype_annotation/CydeApp.java
+++ Spring-01-IoC/src/main/java/com/cydeo/stereotype_annotation/CydeApp.java
@@ -1,4 +1,22 @@
 package com.cydeo.stereotype_annotation;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
 public class CydeApp {
+
+    public static void main(String[] args) {
+
+        ApplicationContext container=new AnnotationConfigApplicationContext(ConfigCourse.class);
+        Java java = container.getBean(Java.class);
+        java.getTeachingHours();
+
+        Agile agile=container.getBean(Agile.class);
+        agile.getTeachingHours();
+
+        Selenium selenium = container.getBean(Selenium.class);
+        selenium.getTeachingHours();
+
+    }
 }
