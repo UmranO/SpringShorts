5b9405c63abee7891b7b8b47e64e9c3b42ca3da2
Umran Ozman <umonal@yahoo.com>
2024-12-25 10:20:17 -0500

    FullTimeMentor Class updated

--- Spring-01-IoC/src/main/java/com/cydeo/bean_annotation/FullTimeMentor.java
+++ Spring-01-IoC/src/main/java/com/cydeo/bean_annotation/FullTimeMentor.java
@@ -1,5 +1,7 @@
 package com.cydeo.bean_annotation;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FullTimeMentor {
     public void createAccount(){
         System.out.println("Full Time Mentor is created");
