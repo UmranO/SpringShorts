606844c025653fe6d429fc5fd7192bf72dc98cfa
Umran Ozman <umonal@yahoo.com>
2024-12-24 20:02:01 -0500

    Selenium Class updated

--- Spring-01-IoC/src/main/java/com/cydeo/stereotype_annotation/Selenium.java
+++ Spring-01-IoC/src/main/java/com/cydeo/stereotype_annotation/Selenium.java
@@ -1,4 +1,7 @@
 package com.cydeo.stereotype_annotation;
 
 public class Selenium {
+    public static void getTeachingHours(){
+        System.out.println("Weekly teaching hours : " + 15);
+    }
 }
