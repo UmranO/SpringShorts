9f5f3fe687ed29a6401a9589bf308c71ce7cd60e
Umran Ozman <umonal@yahoo.com>
2024-12-25 10:34:22 -0500

    Java Class updated

--- Spring-01-IoC/src/main/java/com/cydeo/dependency_injection/Java.java
+++ Spring-01-IoC/src/main/java/com/cydeo/dependency_injection/Java.java
@@ -1,5 +1,7 @@
 package com.cydeo.dependency_injection;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Java {public void getTeachingHours(){
     System.out.println("Weekly teaching hours : " + 20);
 }
