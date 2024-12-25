45b9a1d4d39c85e67c88bafd5cabee20d7cfd7b8
Umran Ozman <umonal@yahoo.com>
2024-12-24 19:40:55 -0500

    CarConfig Class updated

--- Spring-01-IoC/src/main/java/com/cydeo/multi_objects/CarConfig.java
+++ Spring-01-IoC/src/main/java/com/cydeo/multi_objects/CarConfig.java
@@ -6,11 +6,17 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class CarConfig {
 
-    @Bean
+    @Bean (name="c1")
     Car car1(){
       Car c =new Car();
       c.setMake("Audi");
       return c;
+    }
+    @Bean(name="c2")
+    Car c2(){
+        Car c=new Car ();
+        c.setMake("Tesla");
+        return c;
     }
 
 }
