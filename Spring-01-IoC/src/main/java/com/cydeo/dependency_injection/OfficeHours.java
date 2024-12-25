30162a0e5bd426e1b897c3ef99a2dc3dabd09ff9
Umran Ozman <umonal@yahoo.com>
2024-12-25 10:49:50 -0500

    OfficeHours Class updated

--- Spring-01-IoC/src/main/java/com/cydeo/dependency_injection/OfficeHours.java
+++ Spring-01-IoC/src/main/java/com/cydeo/dependency_injection/OfficeHours.java
@@ -1,5 +1,8 @@
 package com.cydeo.dependency_injection;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class OfficeHours {
     public Integer getHours(){
         return 5;
